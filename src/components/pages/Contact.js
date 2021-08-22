4f37bd00bc92360bc2685b8013e8df8ec2b7ed3f
Boba Fett <thugfencer@gmail.com>
2021-08-21 21:09:27 -0700

    Setup basic Contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,22 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Contact.css';
+// import { checkPassword, validateEmail } from '../../utils/helpers.js';
+import { validateEmail } from '../../utils/helpers.js';
 
-export default function Contact() {
-  return (
-    <div>
-      <h1>Contact Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
-    </div>
-  );
+function Contact() {
+     const [email, setEmail] = useState('');
+     const [name, setName] = useState('');
+     //const [password, setPassword] = useState('');
+     const [message, setMessage] = useState('');
+     const [errorMessage, setErrorMessage] = useState('');
+
+     const handleInputChange = (e) => {
+          const { target } = e;
+          const inputType = target.name;
+          const inputValue = target.value;
+          if (inputType === 'email') {
+               setEmail(inputValue);
+          } else if (inputType === 'name') {
+               setName(inputValue);
+          } else {
+               setMessage(inputValue);
+          }
+     };
+
+     const handleFormSubmit = (e) => {
+          e.preventDefault();
+          if (!validateEmail(email) || !name || !message) {
+               setErrorMessage('Email, name, or message is invalid');
+               return;
+          }
+          // if (!checkPassword(password)) {
+          //      setErrorMessage(
+          //           `Choose a more secure password for the account: ${name}`
+          //      );
+          //      return;
+          // }
+          alert(`Hello ${name}`);
+          setName('');
+          setEmail('');
+          setMessage('');
+     };
+
+     return (
+          <div>
+               <p>Hello {name}</p>
+               <form className="form">
+                    <input
+                         value={name}
+                         name="name"
+                         onChange={handleInputChange}
+                         type="text"
+                         placeholder="name"
+                    />
+                    <input
+                         value={email}
+                         name="email"
+                         onChange={handleInputChange}
+                         type="email"
+                         placeholder="email"
+                    />
+                    <input
+                         value={message}
+                         name="message"
+                         onChange={handleInputChange}
+                         type="text"
+                         placeholder="message"
+                    />
+                    <button type="button" onClick={handleFormSubmit}>Submit</button>
+               </form>
+               {errorMessage && (
+                    <div>
+                         <p className="error-text">{errorMessage}</p>
+                    </div>
+               )}
+          </div>
+     );
 }
+export default Contact;
