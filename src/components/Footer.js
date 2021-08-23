c26ee8aba4bc0e8d5f0756f2cbd0d90b9387765d
Boba Fett <thugfencer@gmail.com>
2021-08-22 23:38:46 -0700

    Add third platform to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 import { } from "./Footer.css";
 
 export default function Footer() {
      const myGit = "https://github.com/Fett-Boba";
      const myLinkedin = "https://www.linkedin.com/in/t-s-683927208/";
+     const mySpotify = "https://open.spotify.com/playlist/3Pv56tAsmcFaT4ZI2P26Ld"
      return (
           <footer className="icon-size d-flex justify-content-center bg-red">
                <a className="icon-spacing bg-red text-warning"
@@ -19,6 +21,12 @@ export default function Footer() {
                     rel="noreferrer"
                     href={myLinkedin}>
                     <FontAwesomeIcon icon={faLinkedin} />
+               </a>
+               <a className="icon-spacing bg-red text-warning"
+                    target="_blank"
+                    rel="noreferrer"
+                    href={mySpotify}>
+                    <FontAwesomeIcon icon={faSpotify} />
                </a>
           </footer>
      );
