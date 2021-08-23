e3c787a77cdb0b642d300acb1134cf80a8d08c88
Boba Fett <thugfencer@gmail.com>
2021-08-22 23:06:51 -0700

    Get resume download working, and correct card alignments

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,13 +3,27 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
+import resume from '../../data/tsresume2.pdf'
 import { } from './Resume.css';
 
 export default function Resume() {
      return (
-          <Container className="d-flex justify-content-center mycolors">
-               <Row className="g-0 h-100">
-                    <Col md={4}>
+          <Container className="mycolors">
+
+               <Row className="justify-content-md-center">
+                    <Col md="auto"></Col>
+                    <Col md="auto">
+                         <br />
+                         {/* <button className="btn btn-warning" type="button"><a href={resume}>Download Resume</a></button> */}
+                         <a type="button" className="btn btn-warning btnstyle" href={resume}>Download Resume</a>
+                         <br />
+                         <br />
+                    </Col>
+                    <Col md="auto"></Col>
+               </Row>
+
+               <Row className="justify-content-md-center">
+                    <Col md="auto" className="h-100">
                          <div className="myspacing">
                               <Card className="mb-3 bg-dark rounded-corners mycardsize">
                                    <Card.Body>
@@ -29,7 +43,7 @@ export default function Resume() {
                               </Card>
                          </div>
                     </Col>
-                    <Col md={4}>
+                    <Col md="auto">
                          <div className="myspacing">
                               <Card className="mb-3 bg-dark rounded-corners mycardsize h-100">
                                    <Card.Body>
@@ -54,7 +68,7 @@ export default function Resume() {
                               </Card>
                          </div>
                     </Col>
-                    <Col md={4}>
+                    <Col md="auto">
                          <div className="myspacing">
                               <Card className="mb-3 bg-dark rounded-corners mycardsize h-100">
                                    <Card.Body>
@@ -66,14 +80,13 @@ export default function Resume() {
                                                   <li>JCL</li>
                                                   <li>EzTreive</li>
                                                   <li>Rexx</li>
-                                                  <li>Waapdsut</li>
+                                                  <li>IBM DB2</li>
                                                   <li>SyncSort</li>
                                                   <li>SuperC</li>
                                                   <li>Endevor</li>
-                                                  <li>Changeman</li>
-                                                  <li>NDM</li>
-                                                  <li>z/OS Unix</li>
-                                                  <li>TSO/ISPF</li>
+                                                  <li>ChangeMan</li>
+                                                  <li>Z-Unix</li>
+                                                  <li>z/OS</li>
                                              </ul>
                                         </Card.Text>
                                    </Card.Body>
